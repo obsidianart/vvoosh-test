d9f9b98f04f306e364763c11ba33b467be8256df
stefano solinas <obsidianart@gmail.com>
2016-12-10 15:38:53 +0000

    Update fixture to avoid storying images in the repo

--- fixtures/movies.jsx
+++ fixtures/movies.jsx
@@ -3,36 +3,36 @@ export default [
       "name": "Cars",
       "company": "Pixar",
       "url": "http://www.imdb.com/title/tt0317219/?ref_=nv_sr_1",
-      "image": "movies/cars.jpg"
+      "image": "http://www.obsidianart.com/projects/vvoosh/public/movies/cars.jpg"
   },
   {
       "name": "How to Train Your Dragon",
       "company": "Dreamworks",
       "url": "http://www.imdb.com/title/tt0892769/?ref_=nv_sr_2",
-      "image": "movies/dragon3d.jpg"
+      "image": "http://www.obsidianart.com/projects/vvoosh/public/movies/dragon3d.jpg"
   },
   {
       "name": "Toy Story",
       "company": "Pixar",
       "url": "http://www.imdb.com/title/tt0114709/?ref_=nv_sr_2",
-      "image": "movies/toyStory.jpg"
+      "image": "http://www.obsidianart.com/projects/vvoosh/public/movies/toyStory.jpg"
   },
   {
       "name": "Kung Fu Panda",
       "company": "Dreamworks",
       "url": "http://www.imdb.com/title/tt0441773/?ref_=nv_sr_1",
-      "image": "movies/kungfuPanda.jpg"
+      "image": "http://www.obsidianart.com/projects/vvoosh/public/movies/kungfuPanda.jpg"
   },
   {
       "name": "Finding Nemo",
       "company": "Pixar",
       "url": "http://www.imdb.com/title/tt0266543/?ref_=nv_sr_1",
-      "image": "movies/nemo3d.jpg"
+      "image": "http://www.obsidianart.com/projects/vvoosh/public/movies/nemo3d.jpg"
   },
   {
       "name": "The Croods",
       "company": "Dreamworks",
       "url": "http://www.imdb.com/title/tt0481499/?ref_=nv_sr_1",
-      "image": "movies/croods.jpg"
+      "image": "http://www.obsidianart.com/projects/vvoosh/public/movies/croods.jpg"
   }
 ]
